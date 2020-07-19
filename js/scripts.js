9a65616ee02a0f404139d33601ea9e7dee02032f
virsail <virsaileric@gmail.com>
2020-07-19 23:19:02 +0300

    check names of female gender respectively with the day of birth

--- js/scripts.js
+++ js/scripts.js
@@ -83,3 +83,29 @@ function checkGender(){
             default:
             // console.console.log("Pass");//Test male case
           }
+          break;
+          case gender = "female":
+                  switch(day){
+                    case 0 || -0:
+                      document.getElementById("result").innerHTML = "The day is on a sunday." + "  " + "Your akan name is " + femaleNames[0];
+                    break;
+                    case 1 || -1:
+                      document.getElementById("result").innerHTML = "The day is on a monday." + " " + "Your akan name is " + femaleNames[1];
+                    break;
+                    case 2 || -2:
+                      document.getElementById("result").innerHTML = "The day is on a tuesday." + " " + "Your akan name is " + femaleNames[2];
+                    break;
+                    case 3 || -3:
+                      document.getElementById("result").innerHTML = "The day is on a wednesday." + " " + "Your akan name is " + femaleNames[3];
+                    break;
+                    case 4 || -4:
+                      document.getElementById("result").innerHTML = "The day is on a thursday." + " " + "Your akan name is " + femaleNames[4];
+                    break;
+                    case 5 || -5:
+                      document.getElementById("result").innerHTML = "The day is on a friday." + " " + "Your akan name is " + femaleNames[5];
+                    break;
+                    case 6 || -6:
+                      document.getElementById("result").innerHTML = "The day is on a saturday." + " " + "Your akan name is " + femaleNames[6];
+                    break;
+  
+                }
